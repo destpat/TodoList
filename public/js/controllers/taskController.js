be25a1c8e32bf2ef5075057534e81d2e113283fb
destpat <yoan.ficadiere@gmail.com>
2016-05-30 19:55:16 +0200

    add task, add subTask, delete task, delete all task

--- public/js/controllers/taskController.js
+++ public/js/controllers/taskController.js
@@ -1,17 +1,35 @@
-function taskController($scope) {
-  $scope.names = [];
-  $scope.i = 0;
+function taskController($scope){
+  $scope.tasks = [];
+  $scope.y = 0;
 
+  $scope.add = function (){
+    $scope.tasks.push({
+      todo : $scope.todo,
+      subTasks: []
+    });
+    $scope.todo = '';
+  };
 
-$scope.add = function(){
-  $scope.names.push({
-    todo : $scope.task
-  });
-}
+  $scope.subadd = function (subTasks, index){
+    // console.log($scope);
+    $scope.tasks[index].subTasks.push({
+        todo : subTasks,
+      });
+      console.log( $scope.subTasks);
+  };
 
-$scope.removeTask = function(index){
-  $scope.names.splice(index,1);
-}
-
-
+  $scope.deleteAll = function (){
+    $scope.tasks = [];
+  };
+  $scope.delete = function (index){
+    $scope.tasks.splice(index, 1);
+  };
+  $scope.addFav = function (index){
+    if($scope.y === 0){
+      $scope.y++;
+    }
+    else if($scope.y === 1){
+      $scope.y--;
+    }
+  };
 }
